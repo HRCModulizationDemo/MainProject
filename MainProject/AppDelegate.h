16738feff02d0f1e4aa0fc95d75a386ae2080e2c
rchua <rchua@bainainfo.com>
2017-03-30 20:47:00 +0800

    [feature]first init

---
+++ MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by 华润策 on 2017/3/30.
+//  Copyright © 2017年 HRCModulizationDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by 华润策 on 2017/3/30.
+//  Copyright © 2017年 HRCModulizationDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
